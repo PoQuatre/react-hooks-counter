36e1d890882f5d9dc69f610750ad69e61bcd06db
PoQuatre <leflem.mateo2002@gmail.com>
2022-03-21 12:01:14 +0100

    feat: add counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { useState } from 'react';
+
 export const App = () => {
-  return <h1>Hello World !</h1>;
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => setCount((i) => i + 1);
+  const decrementCount = () => setCount((i) => i - 1);
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={decrementCount}>-</button>
+      <button onClick={incrementCount}>+</button>
+    </>
+  );
 };
